b280c1ab4c1eb5901960ce1e2cec1b83776e9c54
BodhiTreeITService <bodhi.tree.it@foxmail.com>
2024-03-02 13:08:56 +0800

    create Project SourceDemos

---
+++ src/main/java/com/bodhi/sourcedemos/SourceDemosApplication.java
@@ -0,0 +1,13 @@
+package com.bodhi.sourcedemos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SourceDemosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SourceDemosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bodhi/sourcedemos/SourceDemosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bodhi.sourcedemos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SourceDemosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
